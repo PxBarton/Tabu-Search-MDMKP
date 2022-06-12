a2675738cca6597570cfc7685389f7f37aedd274
Paul Barton <pbarton131@gmail.com>
2022-06-12 09:53:35 -0400

    switched problem set

--- Knapsack2.cpp
+++ Knapsack2.cpp
@@ -40,11 +40,11 @@ int main()
 
     // a set of five files for an MDMKP problem
     // two files for the LHS, two for the RHS, one for the objective function coeff's
-    string file1 = "MDMKPcase3&6_all/MDMKP_3_3/LHSknapsack_3_3.csv";
-    string file2 = "MDMKPcase3&6_all/MDMKP_3_3/LHSdemand_3_3.csv";
-    string file3 = "MDMKPcase3&6_all/MDMKP_3_3/RHSknapsack_3_3.csv";
-    string file4 = "MDMKPcase3&6_all/MDMKP_3_3/RHSdemand_3_3.csv";
-    string file5 = "MDMKPcase3&6_all/MDMKP_3_3/ObjCoeffs_3_3.csv";
+    string file1 = "MDMKPcase3&6_all/MDMKP_4_3/LHSknapsack_4_3.csv";
+    string file2 = "MDMKPcase3&6_all/MDMKP_4_3/LHSdemand_4_3.csv";
+    string file3 = "MDMKPcase3&6_all/MDMKP_4_3/RHSknapsack_4_3.csv";
+    string file4 = "MDMKPcase3&6_all/MDMKP_4_3/RHSdemand_4_3.csv";
+    string file5 = "MDMKPcase3&6_all/MDMKP_4_3/ObjCoeffs_4_3.csv";
     
     /*
     string file1 = "MDMKP/LHS_k.txt";
@@ -223,7 +223,7 @@ vector<int> tabuSearch(ProblemCoefficients& coeff, TabuList& tabuList, vector<ve
     tabuList.clearTabuList();
 
     Solution Init(100);
-    int multiplier = 100;
+    int multiplier = multi;
     Init.K_Solution_Gen(26);
     //Init.generate();
 
